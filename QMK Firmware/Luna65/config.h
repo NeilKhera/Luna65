649f128f03c7632fabd0e7ae50768ebc599329e3
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2021-12-07 23:44:53 -0500

    Fixed Broken Bluetooth

--- QMK Firmware/Luna65/config.h
+++ QMK Firmware/Luna65/config.h
@@ -1,28 +1,33 @@
-/* Copyright 2021 Neil Khera
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*
+Copyright 2021 Neil Khera
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include "config_common.h"
 
+#define AdafruitBleCSPin B0
+
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x1024
-#define PRODUCT_ID      0x2048
+#define VENDOR_ID       0x1111
+#define PRODUCT_ID      0x1024
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    NeilKhera
 #define PRODUCT         Luna65
+#define DESCRIPTION     Luna 65 keys with BT
 
 /* key matrix size */
 #define MATRIX_ROWS 6
@@ -33,15 +38,16 @@
  *
  * Change this to how you wired your keyboard
  * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
+tyre
  * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS {F0, F1, F4, F5, F6, B0}
-#define MATRIX_COL_PINS {F7, D1, D2, D3, D5, D6, D7, B7, C7, C6, B6, B5}
+#define MATRIX_COL_PINS { F7, D1, D2, D3, D5, D6, D7, B7, C7, C6, B6, B5 }
+#define MATRIX_ROW_PINS { F0, F1, F4, F5, F6, B4}
+#define UNUSED_PINS
 
-/* COL2ROW, ROW2COL*/
+/* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
 #define DIODE_DIRECTION COL2ROW
 
 #define BACKLIGHT_PIN D0
@@ -54,5 +60,8 @@
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
+
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+#define TAPPING_TERM 100
--- QMK Firmware/Luna65/rules.mk
+++ QMK Firmware/Luna65/rules.mk
@@ -1,19 +1,43 @@
+# MCU name
 MCU = atmega32u4
+
+# Processor frequency
 F_CPU = 16000000
-ARCH = AVR8
-F_USB = $(F_CPU)
-OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328p   USBasp
 BOOTLOADER = caterina
 
+BLUETOOTH_ENABLE = yes
+BLUETOOTH = AdafruitBLE
+
+# Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes           # USB Nkey Rollover
+NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output
+MIDI_ENABLE = no            # MIDI support
+AUDIO_ENABLE = no           # Audio output on port C6
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
+
+UNICODE_ENABLE = no         # Unicode
+UNICODEMAP_ENABLE = no      # ^^
+UCIS_ENABLE = no            # ^^
+
+TAP_DANCE_ENABLE = no
